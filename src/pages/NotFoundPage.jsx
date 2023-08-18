298b7a5f9a4e1a02ef0009248ef28c7de2e41381
Excriyin <xavjon80@gmail.com>
2023-08-18 02:09:42 -0700

    x

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PageNotFound from '../assets/404.jpg';
+import PageNotFound from '../assets/404error.png';
 class NotFoundPage extends React.Component{
     render(){
         return <div>
-            <img src={PageNotFound}  />
+            <img src={PageNotFound} 
+            style="visibility:visible;height:100%;left:0;position:absolute;top:0;width:100%" />
             <p style={{textAlign:"center"}}>
               <Link to="/">Go to Home </Link>
             </p>
