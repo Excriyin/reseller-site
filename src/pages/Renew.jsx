6996e48436e2d12adfcb94124af0d79161b9a7cc
Excriyin <xavjon80@gmail.com>
2023-12-15 06:58:39 -0500

    migrate button

--- src/pages/Renew.jsx
+++ src/pages/Renew.jsx
@@ -12,6 +12,7 @@ export default function Renew() {
   const [password, setPassword] = useState('');
   const [newEmail, setNewEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
+  const [migrate, setMigrate] = useState(false);
   const [country, setCountry] = useState('');
   const [countries, setCountries] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -320,6 +321,7 @@ export default function Renew() {
           country,
           newEmail,
           newPassword,
+          migrate: migrate ? 'yes' : 'no',
         }),
       }).then((res) => res.json());
 
@@ -453,6 +455,43 @@ export default function Renew() {
                       onChange={(e) => setNewPassword(e.target.value)}
                       value={newPassword}
                     />
+                  </div>
+                  <div className='flex items-center mb-4'>
+                    <label
+                      className='relative flex items-center mr-2 rounded-full cursor-pointer'
+                      htmlFor='ripple-off'
+                    >
+                      <input
+                        id='ripple-off'
+                        type='checkbox'
+                        className={`before:content[''] checked:bg-[#1ED760] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 hover:before:opacity-10`}
+                        onChange={() => setMigrate(!migrate)}
+                        checked={migrate}
+                        value={migrate}
+                      />
+                      <span className='absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100'>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          className='h-3.5 w-3.5'
+                          viewBox='0 0 20 20'
+                          fill='currentColor'
+                          stroke='currentColor'
+                          strokeWidth='1'
+                        >
+                          <path
+                            fillRule='evenodd'
+                            d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+                            clipRule='evenodd'
+                          ></path>
+                        </svg>
+                      </span>
+                    </label>
+                    <label
+                      className='mt-px font-medium cursor-pointer select-none'
+                      htmlFor='ripple-off'
+                    >
+                      Migrate playlist
+                    </label>
                   </div>
                   <div className='mb-4'>
                     <label
